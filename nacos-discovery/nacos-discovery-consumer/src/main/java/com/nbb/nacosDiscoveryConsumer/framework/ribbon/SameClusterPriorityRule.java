1e1be89aa10379c7f085319312cb374aac0d5260
胡鹏 <15151873042@163.com>
2023-12-12 17:46:05 +0800

    添加ribbon同集群负载均衡器

--- nacos-discovery/nacos-discovery-consumer/src/main/java/com/nbb/nacosDiscoveryConsumer/framework/ribbon/SameClusterPriorityRule.java
+++ nacos-discovery/nacos-discovery-consumer/src/main/java/com/nbb/nacosDiscoveryConsumer/framework/ribbon/SameClusterPriorityRule.java
@@ -6,14 +6,18 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.AbstractLoadBalancerRule;
 import com.netflix.loadbalancer.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class SameClusterPriorityRule extends AbstractLoadBalancerRule {
 
     @Autowired
     private NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    private static Random random = new Random();
 
 
     @Override
@@ -23,12 +27,18 @@ public class SameClusterPriorityRule extends AbstractLoadBalancerRule {
 
     @Override
     public Server choose(Object key) {
+        // 所有服务实例
         List<NacosServer> allServers = getLoadBalancer().getAllServers().stream().map(item -> (NacosServer)item).collect(Collectors.toList());
 
-        NacosServer nacosServer = allServers.stream()
+        // 集群名称相同的服务实例
+        List<NacosServer> sameClusterServers = allServers.stream()
                 .filter(server -> this.nacosDiscoveryProperties.getClusterName().equals(server.getInstance().getClusterName()))
-                .findFirst()
-                .orElse(allServers.get((int) System.currentTimeMillis() % allServers.size()));
-        return nacosServer;
+                .collect(Collectors.toList());
+
+        if (!CollectionUtils.isEmpty(sameClusterServers)) {
+            return sameClusterServers.get(random.nextInt(sameClusterServers.size()));
+        }
+
+        return allServers.get(random.nextInt(allServers.size()));
     }
 }
